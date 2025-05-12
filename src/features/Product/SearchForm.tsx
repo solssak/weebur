9cb79e48e20c8a8f73cf78d353ee97f91a87435b
solssak <gswoong1012@gmail.com>
2025-05-13 00:25:59 +0900

    feat: 새로고침시에도 검색 리스트가 유지되는 기능 구현
    - 사용자가 입력한 검색 쿼리를 로컬 스토리지에 저장하고, 컴포넌트가 마운트될 때 저장된 쿼리를 불러오는 로직 구현

--- src/features/Product/SearchForm.tsx
+++ src/features/Product/SearchForm.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useEffect } from 'react';
+
 interface SearchFormProps {
   searchQuery: string;
   onSearchQueryChange: (query: string) => void;
@@ -9,13 +13,23 @@ export const SearchForm = ({
   onSearchQueryChange,
   onSearch,
 }: SearchFormProps) => {
+  useEffect(() => {
+    const savedQuery = localStorage.getItem('searchQuery');
+    if (savedQuery) onSearchQueryChange(savedQuery);
+  }, []);
+
+  const handleSearchQueryChange = (query: string) => {
+    onSearchQueryChange(query);
+    localStorage.setItem('searchQuery', query);
+  };
+
   return (
     <form onSubmit={onSearch} className="m-6 flex justify-end">
       <div className="flex gap-2">
         <input
           type="text"
           value={searchQuery}
-          onChange={(e) => onSearchQueryChange(e.target.value)}
+          onChange={(e) => handleSearchQueryChange(e.target.value)}
           placeholder="Search Here..."
           className="flex-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
