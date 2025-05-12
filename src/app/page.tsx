ddcd6050642cfac56b3891680712b9a8738e96a8
solssak <gswoong1012@gmail.com>
2025-05-12 22:27:59 +0900

    refactor: ProductItems 컴포넌트에서 서버 데이터 fetch 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 import { ProductItems } from '@/features/Product/ProductItems';
 
 export default async function Home() {
-  const getProduct = await fetch('https://dummyjson.com/products');
-  const product = await getProduct.json();
-
-  return <ProductItems product={product} />;
+  return <ProductItems />;
 }
